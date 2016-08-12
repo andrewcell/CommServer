e1093dea37859b851b2bc5b7c99c779362aa8bca
andrewcell <andrew@iryu.kr>
2016-08-13 00:04:32 +0900

    Add Json Connection Test Function

--- CommClient.CSharp.Library/Json.cs
+++ CommClient.CSharp.Library/Json.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Socket;
+using Newtonsoft.Json;
 
 namespace CommClient.CSharp.Library
 {
     class Json
     {
-        
+        private static Random random = new Random();
+        public string Login(string id, string password)
+        {
+
+        }
+        public string ConnectTest()
+        {
+            JsonData datas = new JsonData();
+            datas.data = "Connection Test";
+            string JsonString = JsonConvert.SerializeObject(datas);
+            return JsonString;
+        }
+    }
+    public class JsonData
+    {
+        public string data { get; set; }
     }
 }
