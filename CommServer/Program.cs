0af79aa29ce4fba7a17810f63d8e86478b9f1155
andrewcell <andrew@iryu.kr>
2016-08-13 13:42:43 +0900

    Add Client process method

--- CommServer/Program.cs
+++ CommServer/Program.cs
@@ -8,24 +8,62 @@ using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 using System.Net.Security;
 using System.IO;
+using System.Security.Authentication;
 
 namespace CommServer
 {
     class Program
     {
+        
+        
         static void Main(string[] args)
-        {   
+        {
+            HttpListener http = new HttpListener();
+            http.
             TcpListener listner = new TcpListener(IPAddress.Any, 443);
             listner.Start();
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            TcpClient clientsock = listner.AcceptTcpClient();
+
             X509Certificate2 certificate = new X509Certificate2("c:/users/andrew/sample.pfx","1");
-            using (SslStream ssl = new SslStream(clientsock.GetStream()))
+            while (true)
             {
-                ssl.AuthenticateAsServer(certificate);
-                Console.WriteLine(ReadMessage(ssl));
+                TcpClient clientsock = listner.AcceptTcpClient();
+                proccess(clientsock,certificate);
             }
-            Console.Beep();
+        }
+        static void proccess(TcpClient client, X509Certificate2 cert)
+        {
+            SslStream ssl = new SslStream(client.GetStream(), false);
+            try
+            {
+                ssl.AuthenticateAsServer(cert, false, SslProtocols.Tls12, true);
+                DisplaySecurityLevel(ssl);
+                DisplaySecurityServices(ssl);
+                DisplayCertificateInformation(ssl);
+                DisplayStreamProperties(ssl);
+                byte[] message = Encoding.UTF8.GetBytes(BuildString());
+                ssl.Write(message);
+                string Message = ReadMessage(ssl);
+                Console.WriteLine("{0}", Message);
+             message = Encoding.UTF8.GetBytes(BuildString());
+                ssl.Write(message);
+            }
+            catch (AuthenticationException e)
+            {
+                Console.WriteLine(e.Message);
+                ssl.Close();
+                client.Close();
+            }
+            finally
+            {
+                ssl.Close();
+                client.Close();
+            }
+        }
+        static byte[] Send(string Message)
+        {
+            byte[] atk = Encoding.UTF8.GetBytes(Message);
+            return atk;
         }
         static string ReadMessage(SslStream sslStream)
         {
@@ -50,6 +88,76 @@ namespace CommServer
             } while (bytes != 0);
 
             return messageData.ToString();
+        }
+        static string BuildString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("HTTP/1.1 200 OK");
+            //sb.AppendLine("Date: " + DateTime.Now.ToString());
+            sb.AppendLine("Content-Type: text/html; charset=UTF-8");
+            sb.AppendLine("Transfer-Encoding: chunked");
+            sb.AppendLine("Connection: keep-alive");
+            sb.AppendLine("");
+            sb.AppendLine("WARNING");
+            return sb.ToString();
+        }
+
+
+
+
+        static void DisplaySecurityLevel(SslStream stream)
+        {
+            Console.WriteLine("Cipher: {0} strength {1}", stream.CipherAlgorithm, stream.CipherStrength);
+            Console.WriteLine("Hash: {0} strength {1}", stream.HashAlgorithm, stream.HashStrength);
+            Console.WriteLine("Key exchange: {0} strength {1}", stream.KeyExchangeAlgorithm, stream.KeyExchangeStrength);
+            Console.WriteLine("Protocol: {0}", stream.SslProtocol);
+        }
+        static void DisplaySecurityServices(SslStream stream)
+        {
+            Console.WriteLine("Is authenticated: {0} as server? {1}", stream.IsAuthenticated, stream.IsServer);
+            Console.WriteLine("IsSigned: {0}", stream.IsSigned);
+            Console.WriteLine("Is Encrypted: {0}", stream.IsEncrypted);
+        }
+        static void DisplayStreamProperties(SslStream stream)
+        {
+            Console.WriteLine("Can read: {0}, write {1}", stream.CanRead, stream.CanWrite);
+            Console.WriteLine("Can timeout: {0}", stream.CanTimeout);
+        }
+        static void DisplayCertificateInformation(SslStream stream)
+        {
+            Console.WriteLine("Certificate revocation list checked: {0}", stream.CheckCertRevocationStatus);
+
+            X509Certificate localCertificate = stream.LocalCertificate;
+            if (stream.LocalCertificate != null)
+            {
+                Console.WriteLine("Local cert was issued to {0} and is valid from {1} until {2}.",
+                    localCertificate.Subject,
+                    localCertificate.GetEffectiveDateString(),
+                    localCertificate.GetExpirationDateString());
+            }
+            else
+            {
+                Console.WriteLine("Local certificate is null.");
+            }
+            // Display the properties of the client's certificate.
+            X509Certificate remoteCertificate = stream.RemoteCertificate;
+            if (stream.RemoteCertificate != null)
+            {
+                Console.WriteLine("Remote cert was issued to {0} and is valid from {1} until {2}.",
+                    remoteCertificate.Subject,
+                    remoteCertificate.GetEffectiveDateString(),
+                    remoteCertificate.GetExpirationDateString());
+            }
+            else
+            {
+                Console.WriteLine("Remote certificate is null.");
+            }
+        }
+        private static void DisplayUsage()
+        {
+            Console.WriteLine("To start the server specify:");
+            Console.WriteLine("serverSync certificateFile.cer");
+            Environment.Exit(1);
         }
     }
 }
