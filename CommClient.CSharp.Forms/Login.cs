21ce66b68cea74b45b7f55f3c9d4a91854e797be
andrewcell <andrew@iryu.kr>
2016-08-20 20:54:57 +0900

    Change default form name to login

--- CommClient.CSharp.Forms/Login.cs
+++ CommClient.CSharp.Forms/Login.cs
@@ -9,11 +9,21 @@ using System.Windows.Forms;
 
 namespace CommClient.CSharp.Forms
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
