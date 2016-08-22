c526115c6c7cb1f06c411479829b5979617b0c32
andrewcell <andrew@iryu.kr>
2016-08-22 22:37:40 +0900

    Add Data response Function (WIP)

--- CommServer.Library/CommServer.Library.cpp
+++ CommServer.Library/CommServer.Library.cpp
@@ -2,5 +2,42 @@
 //
 
 #include "stdafx.h"
+#include <string>
 
 
+using namespace std;
+namespace Library
+{
+	
+	union Information
+	{
+		bool XMLUse;
+		int EncryptionType;
+
+	};
+	Information cls;
+	bool SetStyle(char Encryptiontype, bool XMLUse)
+	{
+		Information cl;
+		cl.EncryptionType = Encryptiontype;
+		cl.XMLUse = XMLUse;
+		return false;
+	}
+	class DataReturn
+	{
+	private:
+		string data;
+
+	public:
+		string dataReturn(string);
+
+	};
+	string DataReturn::dataReturn(string Data)
+	{
+		if (data == "GetServerStyle")
+		{
+			string parse = to_string(cls.EncryptionType) + ";" + to_string(cls.XMLUse);
+			return parse;
+		}
+	}
+}
