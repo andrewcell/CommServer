e5407e6b6604a4fcc1866bbbc9927f1fe6edfa2a
andrewcell <andrew@iryu.kr>
2016-08-22 23:25:12 +0900

    Change C++ Library Project Type

--- CommServer.Library/CommServer.Library.cpp
+++ CommServer.Library/CommServer.Library.cpp
@@ -1,43 +1,6 @@
-// CommServer.Library.cpp : Defines the exported functions for the DLL application.
-//
+// This is the main DLL file.
 
 #include "stdafx.h"
-#include <string>
 
+#include "CommServer.Library.h"
 
-using namespace std;
-namespace Library
-{
-	
-	union Information
-	{
-		bool XMLUse;
-		int EncryptionType;
-
-	};
-	Information cls;
-	bool SetStyle(char Encryptiontype, bool XMLUse)
-	{
-		Information cl;
-		cl.EncryptionType = Encryptiontype;
-		cl.XMLUse = XMLUse;
-		return false;
-	}
-	class DataReturn
-	{
-	private:
-		string data;
-
-	public:
-		string dataReturn(string);
-
-	};
-	string DataReturn::dataReturn(string Data)
-	{
-		if (data == "GetServerStyle")
-		{
-			string parse = to_string(cls.EncryptionType) + ";" + to_string(cls.XMLUse);
-			return parse;
-		}
-	}
-}
---
+++ CommServer.Library/CommServer.Library.h
@@ -0,0 +1,60 @@
+// CommServer.Library.h
+
+#pragma once
+
+#include <string>
+
+using namespace System;
+using namespace std;
+
+namespace CommServerLibrary {
+	union Information
+	{
+		bool XMLUse;
+		int EncryptionType;
+		
+	};
+	
+	Information cls;
+	public ref class DataReturn
+	{
+	public:
+		String^ dataReturn(String^);
+		bool SetStyle(char, bool);
+
+	};
+	String^ DataReturn::dataReturn( String^ Data)
+	{
+		if (Data == "GetServerStyle")
+		{
+			String^ parse = "";//cls.EncryptionType.ToString() + ";" + cls.XMLUse.ToString();
+			return parse;
+		}
+	}
+	bool DataReturn::SetStyle(char Encryptiontype, bool XMLUse)
+	{
+		Information cl;
+		cl.EncryptionType = Encryptiontype;
+		cl.XMLUse = XMLUse;
+		return false;
+	}
+	
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
+
+/*
+
+#include "stdafx.h"
+#include <string>
+
+
+using namespace std;
+namespace Library
+{
+
+	
+}*/
